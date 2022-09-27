692bab55e25f37af4cc884b3cacd31275687869a
Angad Kambli <angadkambli@gmail.com>
2022-09-28 00:35:23 +0530

    particles now follow mouse

--- particlerunner.js
+++ particlerunner.js
@@ -1,10 +1,97 @@
-var js_wrapped_initialize_particle_system = Module.cwrap("initialize_particle_system", null, ["number", "number", "number"]);
+let initialize_particle_system = Module.cwrap("initialize_particle_system", null, ["number", "number", "number"]);
+let update_particle_system = Module.cwrap("update_particle_system", null, ["number", "number", "number"]);
+let n = 100;
+let colors_array, positions_array;
+const start = async () => {
+    // Instantiate the wasm.
+    // const res = await WebAssembly.instantiate(buf, {})
+  
+    // // Get the function out of the exports.Module.HEAP32.buffer
+    // const { sumArrayInt32, memory } = res.instance.exports
+    var colors_ptr = Module._malloc(4*n*3);
+    var positions_ptr = Module._malloc(4*n*2);
+    colors_array = new Float32Array(Module.HEAP32.buffer, colors_ptr, n*3);
+    positions_array = new Float32Array(Module.HEAP32.buffer, positions_ptr, n*2);
 
-const start = () => {
-    var n = 10;
-    var colors = Module._malloc(4 * n * 3);
-    var positions = Module._malloc(4 * n * 2);
+    initialize_particle_system(n, colors_array.byteOffset, positions_array.byteOffset);
+    addParticles();
+}
 
-    js_wrapped_initialize_particle_system(n, colors, positions);
-    console.log("reached")
+const showArrays = () => {
+    console.log(colors_array);
+    console.log(positions_array);
+}
+
+var mousePos;
+document.onmousemove = handleMouseMove;
+function handleMouseMove(event) {
+    var dot, eventDoc, doc, body, pageX, pageY;
+
+    event = event || window.event; // IE-ism
+
+    // If pageX/Y aren't available and clientX/Y are,
+    // calculate pageX/Y - logic taken from jQuery.
+    // (This is to support old IE)
+    if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+
+        event.pageX = event.clientX +
+          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+          (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+          (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+          (doc && doc.clientTop  || body && body.clientTop  || 0 );
+    }
+
+    mousePos = {
+        x: event.pageX,
+        y: event.pageY
+    };
+}
+
+const addParticles = () => {
+    let particles = document.getElementsByClassName("particles")
+    Array.from(particles).map((val) => {
+        val.remove();
+    })
+    Array.from(Array(n).keys()).map((index)=>{
+        const r = colors_array[index*3];
+        const g = colors_array[index*3 + 1];
+        const b = colors_array[index*3 + 2];
+        const div = document.createElement("div");
+        div.id = "particle_"+index.toString();
+        div.className = "particles";
+        div.style.borderRadius = "50%";
+        div.style.backgroundColor = "rgb(" + r.toString() + ", " + g.toString() + ", " + b.toString() + ")";
+        div.style.position = "absolute";
+        div.style.height = "5px";
+        div.style.width = "5px";
+        div.style.left = positions_array[index*2];
+        div.style.top = positions_array[index*2 + 1];
+        document.body.appendChild(div);
+    })
+    updateParticles(0)
+}
+
+const updateParticles = async (delta) => {
+    // console.log("delta", delta);
+    // console.log(mousePos?.x, mousePos?.y)
+    if(mousePos)
+    {
+        await update_particle_system(mousePos.x, mousePos.y, delta);
+        Array.from(Array(n).keys()).map((index)=>{
+            const div = document.getElementById("particle_"+index.toString());
+            const r = colors_array[index*3];
+            const g = colors_array[index*3 + 1];
+            const b = colors_array[index*3 + 2];
+            div.style.backgroundColor = "rgb(" + r.toString() + ", " + g.toString() + ", " + b.toString() + ")";
+            div.style.left = positions_array[index*2];
+            div.style.top = positions_array[index*2 + 1];
+        })
+    }
+    
+
+    requestAnimationFrame(updateParticles)
 }
--- particles.cpp
+++ particles.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <emscripten.h> // note we added the emscripten header
 #include <stdlib.h>
+#include <cmath>
 class particle_system
 {
-    int* colors;
+    float* colors;
     float* positions;
+    float* masses;
+    float* speeds;
     int n;
+    
 
     // base color
-    int base_r = 40;
-    int base_g = 40;
-    int base_b = 100;
+    int base_r = 109;
+    int base_g = 227;
+    int base_b = 18;
+
+    int target_r = 227;
+    int target_g = 18;
+    int target_b = 178;
+
+    //constants
+    float force_multiplier = 20;
+    float max_speed = 0.5; 
 
     void set_random_colors()
     {
@@ -25,6 +37,24 @@ class particle_system
         }
     }
 
+    void set_masses()
+    {
+        masses = (float*)malloc(n*sizeof(int));
+        for(int i = 0; i < n; i++)
+        {
+            masses[i] = rand()%100;
+        }
+    }
+
+    void set_speeds()
+    {
+        speeds = (float*)malloc(n*sizeof(int)*2);
+        for(int i = 0; i < 2*n; i++)
+        {
+            speeds[i] = 0;
+        }
+    }
+
     void set_random_positions()
     {
         for(int i = 0; i < n; i++)
@@ -35,21 +65,91 @@ class particle_system
             positions[k] = rand() % 100;
         }
     }
+
+    float sigmoid(float x)
+    {
+        float e_x = std::expf(-x);
+        e_x += 1;
+        return 1/e_x;
+    }
+
+    float map_inf_to_1(float x)
+    {
+        return ((2 * sigmoid(x)) - 1); 
+    }
+
+    float smooth_step(float from, float to, float x)
+    {
+        return (from * (1 - x)) + (to * x);
+    }
+
 public:
-    particle_system(int p_n, int* p_colors, float* p_positions)
+    particle_system(int p_n, float* p_colors, float* p_positions)
     {
         n = p_n;
         colors = p_colors;
         positions = p_positions;
         set_random_colors();
         set_random_positions();
+        set_masses();
+        set_speeds();
+    }
+    void update_particle_system(int target_x, int target_y, float delta)
+    {
+        delta = delta/1000;
+        for(int i = 0; i < n; i++)
+        {
+            float x = positions[i*2];
+            float y = positions[i*2 + 1];
+            // std::cout << "positions " << positions[i*2] << " " << positions[i*2 + 1] << "\n";
+            float dx = target_x-x;
+            float dy = target_y-y;
+            // std::cout << "dx dy " << dx << " " << dy << "\n";
+            float distance = std::sqrt(dx*dx + dy*dy);
+            
+            float mapped_d = map_inf_to_1(distance);
+            colors[i*3] = smooth_step(base_r, target_r, mapped_d);
+            colors[i*3 + 1] = smooth_step(base_g, target_g, mapped_d);
+            colors[i*3 + 2] = smooth_step(base_b, target_b, mapped_d);
+
+            if(distance < 5)
+            {
+                distance = 5;
+            }
+            // std::cout << "distance " << distance << "\n";
+            float force = force_multiplier/distance;
+            // std::cout << "force " << force << "\n";
+            float acceleration = force/masses[i];
+            // std::cout << "acceleration " << acceleration << "\n";
+            // std::pair<float, float> normalized = normalize(dx, dy);
+            float l = std::sqrt(dx*dx + dy*dy);
+            float ndx = float(dx)/l;
+            float ndy = float(dy)/l;
+            // std::cout << "ndx ndy " << ndx << " " << ndy << "\n";
+            float acc_x = acceleration * ndx;
+            float acc_y = acceleration * ndy;
+            // std::cout << "acc_x acc_y " << acc_x << " " << acc_y << "\n";
+            positions[i*2] = positions[i*2] + speeds[i*2]*delta;
+            positions[i*2 + 1] = positions[i*2 + 1] + speeds[i*2 + 1]*delta;
+            // std::cout << "positions " << positions[i*2] << " " << positions[i*2 + 1] << "\n";
+            speeds[i*2] = speeds[i*2] + acc_x*delta;
+            speeds[i*2 + 1] = speeds[i*2 + 1] + acc_y*delta;
+            // std::cout << "speeds " << speeds[i*2] << " " << speeds[i*2 + 1] << "\n";
+            float length_speed = std::sqrt(speeds[i*2]*speeds[i*2] + speeds[i*2 + 1]*speeds[i*2 + 1]);
+            if(length_speed > max_speed)
+            {
+                speeds[i*2] = (speeds[i*2] / length_speed) * max_speed;
+                speeds[i*2 + 1] = (speeds[i*2 + 1] / length_speed) * max_speed;
+            }
+        }
     }
 };
 particle_system* p_s;
 extern "C" {
-void initialize_particle_system(int p_n, int* p_colors, float* p_positions);
+void initialize_particle_system(int p_n, float* p_colors, float* p_positions);
+void update_particle_system(int target_x, int target_y, float delta);
 }
-void initialize_particle_system(int p_n, int* p_colors, float* p_positions)
+void initialize_particle_system(int p_n, float* p_colors, float* p_positions)
 {
     p_s = new particle_system(p_n, p_colors, p_positions);
     EM_ASM(
@@ -57,6 +157,10 @@ void initialize_particle_system(int p_n, int* p_colors, float* p_positions)
         console.log("Particle system initiated");
         // end of javascript code
     );
+}
+void update_particle_system(int target_x, int target_y, float delta)
+{
+    p_s->update_particle_system(target_x, target_y, delta);
 }
 
 int main(){
